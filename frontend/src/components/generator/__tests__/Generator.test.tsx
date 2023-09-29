d00039f3dc092879325fff5872dce9380434d47f
Yanfei Xue <yxue@expediagroup.com>
2023-09-29 23:58:42 +0800

    test: improve front-end UT coverage

--- frontend/src/components/generator/__tests__/Generator.test.tsx
+++ frontend/src/components/generator/__tests__/Generator.test.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { mocked } from 'jest-mock'
 import { render, screen } from '@testing-library/react';
 import { Generator } from '../Generator';
+import { ServiceError, ServiceLoading, useApi } from '../../../common';
 import { GeneratorDigits } from '../GeneratorDigits'
-import { useApi } from '../../../common'
 
 jest.mock('../GeneratorDigits')
 jest.mock('../../../common')
+
 
 describe('<Generator />', () => {
   const onGeneratedMock = jest.fn()
@@ -30,5 +31,30 @@ describe('<Generator />', () => {
     expect(button).toBeInTheDocument()
     expect(button?.innerHTML).toContain('Generate Token')
     expect(button).toBeDisabled()
+  });
+
+  it('should render error banner if there is a service error', () => {
+    const MockedServiceError = <div data-testid="service-error" />
+    mocked(useApi).mockReturnValue([{ status: 'ERROR' }, mockedRequest])
+    mocked(ServiceError).mockReturnValue(MockedServiceError)
+
+    render(<Generator onGenerated={onGeneratedMock} />);
+    expect(screen.getByTestId('service-error')).toBeInTheDocument()
+  });
+
+  it('should render loader if it is fetching data', () => {
+    const MockedServiceLoading = <div data-testid="service-loading" />
+    mocked(useApi).mockReturnValue([{ status: 'LOADING' }, mockedRequest])
+    mocked(ServiceLoading).mockReturnValue(MockedServiceLoading)
+
+    render(<Generator onGenerated={onGeneratedMock} />);
+    expect(screen.getByTestId('service-loading')).toBeInTheDocument()
+  });
+
+  it('should render token if it is successful to fetch data', () => {
+    mocked(useApi).mockReturnValue([{ status: 'SUCCESS', data: {token: '123'} }, mockedRequest])
+
+    render(<Generator onGenerated={onGeneratedMock} />);
+    expect(screen.getByText(/Generated Token: 123/)).toBeInTheDocument()
   });
 })
--- frontend/src/components/validator/__tests__/Validator.test.tsx
+++ frontend/src/components/validator/__tests__/Validator.test.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { mocked } from 'jest-mock';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Validator } from '../Validator';
-
-import { useApi } from '../../../common'
+import { ValidationMessage } from '../ValidatorMessage';
+import { useApi, ServiceError, ServiceLoading } from '../../../common'
 
 jest.mock('../../../common')
+jest.mock('../ValidatorMessage')
 
 describe('<Validator />', () => {
   const token = "3344"
@@ -13,6 +14,10 @@ describe('<Validator />', () => {
 
   beforeEach(() => {
     mocked(useApi).mockReturnValue([{ status: 'IDLE' }, mockedRequest])
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
   })
 
   it('should render a disabled button', () => {
@@ -36,4 +41,32 @@ describe('<Validator />', () => {
     fireEvent.click(screen.getByRole('button'))
     expect(mockedRequest).toBeCalledWith(`/api/validator/tokens/${token}/status`, { method: 'GET' })
   })
+
+  it('should render error banner if there is a service error', () => {
+    const MockedServiceError = <div data-testid="service-error" />
+    mocked(useApi).mockReturnValue([{ status: 'ERROR' }, mockedRequest])
+    mocked(ServiceError).mockReturnValue(MockedServiceError)
+
+    render(<Validator token={token} />);
+    expect(screen.getByTestId('service-error')).toBeInTheDocument()
+  });
+
+  it('should render loader if it is calling to validate token', () => {
+    const MockedServiceLoading = <div data-testid="service-loading" />
+    mocked(useApi).mockReturnValue([{ status: 'LOADING' }, mockedRequest])
+    mocked(ServiceLoading).mockReturnValue(MockedServiceLoading)
+
+    render(<Validator token={token} />);
+    expect(screen.getByTestId('service-loading')).toBeInTheDocument()
+  });
+
+  it('should render token validation result if it is successful to fetch data', () => {
+    const MockedValidationMessage = <div data-testid="validation-message" />
+    mocked(useApi).mockReturnValue([{ status: 'SUCCESS', data: {valid: true} }, mockedRequest])
+    mocked(ValidationMessage).mockReturnValue(MockedValidationMessage)
+
+    render(<Validator token={token} />);
+    fireEvent.click(screen.getByRole('button'))
+    expect(screen.getByTestId('validation-message')).toBeInTheDocument()
+  });
 })
